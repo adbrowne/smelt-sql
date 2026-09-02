75a7aacbca57de1e5ca4e31d2848e8f7f0817987
github-action-benchmark <github@users.noreply.github.com>
2026-09-02 10:03:28 +0000

    add Smelt Throughput Benchmarks (customBiggerIsBetter) benchmark result for 21464dd2c2c2d0bf7cffd73363a70a6808a8f8be

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1788343405644,
+  "lastUpdate": 1788343408566,
   "repoUrl": "https://github.com/adbrowne/smelt-sql",
   "entries": {
     "Smelt Latency Benchmarks": [
@@ -94,6 +94,37 @@ window.BENCHMARK_DATA = {
             "name": "Parser / Batch (1000)",
             "value": 11.258612,
             "unit": "ms"
+          }
+        ]
+      }
+    ],
+    "Smelt Throughput Benchmarks": [
+      {
+        "commit": {
+          "author": {
+            "email": "brownie@brownie.com.au",
+            "name": "Andrew Browne",
+            "username": "adbrowne"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "21464dd2c2c2d0bf7cffd73363a70a6808a8f8be",
+          "message": "Merge pull request #183 from adbrowne/annes-words\n\nAnnes words",
+          "timestamp": "2026-09-02T19:59:15+10:00",
+          "tree_id": "e2175fb23771da74706e782aceff19d108f112dc",
+          "url": "https://github.com/adbrowne/smelt-sql/commit/21464dd2c2c2d0bf7cffd73363a70a6808a8f8be"
+        },
+        "date": 1788343407675,
+        "tool": "customBiggerIsBetter",
+        "benches": [
+          {
+            "name": "Parser / Throughput",
+            "value": 30.61887202436677,
+            "unit": "MB/s"
           }
         ]
       }
