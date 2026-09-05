178beeabe105ddafd5b9e132d374aa25aa621261
github-action-benchmark <github@users.noreply.github.com>
2026-09-05 01:42:16 +0000

    add Smelt Throughput Benchmarks (customBiggerIsBetter) benchmark result for 128edb54a48d117f03175a95c863218683c32d89

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1788572532909,
+  "lastUpdate": 1788572536301,
   "repoUrl": "https://github.com/adbrowne/smelt-sql",
   "entries": {
     "Smelt Latency Benchmarks": [
@@ -94,6 +94,37 @@ window.BENCHMARK_DATA = {
             "name": "Parser / Batch (1000)",
             "value": 13.707459,
             "unit": "ms"
+          }
+        ]
+      }
+    ],
+    "Smelt Throughput Benchmarks": [
+      {
+        "commit": {
+          "author": {
+            "email": "brownie@brownie.com.au",
+            "name": "Andrew Browne",
+            "username": "adbrowne"
+          },
+          "committer": {
+            "email": "brownie@brownie.com.au",
+            "name": "Andrew Browne",
+            "username": "adbrowne"
+          },
+          "distinct": true,
+          "id": "128edb54a48d117f03175a95c863218683c32d89",
+          "message": "docs(research): ten candidate directions for smelt, recommend explain-the-diff",
+          "timestamp": "2026-09-05T11:37:35+10:00",
+          "tree_id": "18800dcaf68f96b64361b13261c7d7cc40e4e0a2",
+          "url": "https://github.com/adbrowne/smelt-sql/commit/128edb54a48d117f03175a95c863218683c32d89"
+        },
+        "date": 1788572535052,
+        "tool": "customBiggerIsBetter",
+        "benches": [
+          {
+            "name": "Parser / Throughput",
+            "value": 25.148789429171373,
+            "unit": "MB/s"
           }
         ]
       }
